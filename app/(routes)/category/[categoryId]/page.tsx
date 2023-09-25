c09767bf13e6452bb2ef89f3dca35dc253d406f5
waelmando0 <59392618+waelmando0@users.noreply.github.com>
2023-09-25 18:13:21 +0300

    fixed error

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -36,28 +36,30 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
   const category = await getCategory(params.categoryId)
 
   return (
-    <section className="container">
-      <div className="py-10">
-        <Billboard data={category.billboard} />
-        <div className="pb-24 py-8">
-          <div className="grid grid-cols-7 lg:gap-x-8">
-            <MobileFilters sizes={sizes} colors={colors} />
-            <div className="hidden lg:block col-span-2">
-              <Filter valueKey="sizeId" name="Sizes" data={sizes} />
-              <Filter valueKey="colorId" name="Colors" data={colors} />
-            </div>
-            <div className="mt-6 lg:col-span-5 col-span-7 lg:mt-0">
-              {products.length === 0 && <NoResults />}
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-                {products.map((item) => (
-                  <ProductCard key={item.id} data={item} />
-                ))}
+    <div className="bg-white">
+      <section className="container">
+        <div className="py-10">
+          <Billboard data={category.billboard} />
+          <div className="px-4 sm:px-6 lg:px-8 pb-24">
+            <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+              <MobileFilters sizes={sizes} colors={colors} />
+              <div className="hidden lg:block">
+                <Filter valueKey="sizeId" name="Sizes" data={sizes} />
+                <Filter valueKey="colorId" name="Colors" data={colors} />
+              </div>
+              <div className="mt-6 lg:col-span-4 lg:mt-0">
+                {products.length === 0 && <NoResults />}
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                  {products.map((item) => (
+                    <ProductCard key={item.id} data={item} />
+                  ))}
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   )
 }
 
